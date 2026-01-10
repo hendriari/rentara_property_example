7b819f06d3f358e9033296f1fbd4c1959a50c463
hendriari <106725048+hendriari@users.noreply.github.com>
2026-01-10 13:22:42 +0700

    add app color and app padding

--- lib/src/core/rentara_property_app.dart
+++ lib/src/core/rentara_property_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rentara_property_clone/src/core/routes/app_routes.dart';
+import 'package:rentara_property_clone/src/core/theme/app_colors.dart';
 
 class RentaraPropertyApp extends StatelessWidget {
   const RentaraPropertyApp({super.key});
@@ -16,8 +17,13 @@ class RentaraPropertyApp extends StatelessWidget {
           theme: ThemeData(
             useMaterial3: true,
             textTheme: ThemeData.light().textTheme.apply(fontFamily: "Manrope"),
-
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: AppColors.primaryColor600,
+            ),
           ),
+          debugShowCheckedModeBanner: false,
+          showPerformanceOverlay: true,
+          themeMode: ThemeMode.light,
           routerConfig: AppRoutes.route(),
           supportedLocales: const [Locale('id'), Locale('en')],
           localizationsDelegates: const [
